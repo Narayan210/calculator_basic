65d2a78de2057c8ea1e7215dd004eefc88af6e7e
Narayan <141340370+Narayan210@users.noreply.github.com>
2023-08-04 22:26:24 +0545

    Update calculatorbasic.py

--- calculatorbasic.py
+++ calculatorbasic.py
@@ -1,12 +1,15 @@
 # making calculator using simple method
 
-
+# input first number
 first_number=float(input("enter first number; "))
 
+#input operator
 operator=input("enter operator(+,-,*,/); ")
 
+#input second number
 second_number=float(input("enter secound number; "))
 
+# enter condition and print accordingly
 if operator== "+":
     print(first_number + second_number)
 
